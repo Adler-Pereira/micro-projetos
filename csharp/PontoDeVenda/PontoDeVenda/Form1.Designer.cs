da9439b7a8fa9ea2d0256eb89350c5721a8eb447
adler.lima AL. <adler.lima@tecdetbrg.com.br>
2026-05-14 11:28:52 -0300

    Criação do projeto PontoDeVenda e construção de seu layout

---
+++ csharp/PontoDeVenda/PontoDeVenda/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace PontoDeVenda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btCadVenda = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btCadCliente = new System.Windows.Forms.Button();
+            this.btCadProduto = new System.Windows.Forms.Button();
+            this.btEdiProduto = new System.Windows.Forms.Button();
+            this.btEdiVenda = new System.Windows.Forms.Button();
+            this.btEdiCliente = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(9, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Produtos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(9, 267);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Vendas";
+            // 
+            // btCadVenda
+            // 
+            this.btCadVenda.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btCadVenda.Location = new System.Drawing.Point(0, 292);
+            this.btCadVenda.Name = "btCadVenda";
+            this.btCadVenda.Size = new System.Drawing.Size(141, 23);
+            this.btCadVenda.TabIndex = 3;
+            this.btCadVenda.Text = "Cadastro";
+            this.btCadVenda.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(9, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Clientes";
+            // 
+            // btCadCliente
+            // 
+            this.btCadCliente.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btCadCliente.Location = new System.Drawing.Point(0, 190);
+            this.btCadCliente.Name = "btCadCliente";
+            this.btCadCliente.Size = new System.Drawing.Size(141, 23);
+            this.btCadCliente.TabIndex = 4;
+            this.btCadCliente.Text = "Cadastro";
+            this.btCadCliente.UseVisualStyleBackColor = true;
+            // 
+            // btCadProduto
+            // 
+            this.btCadProduto.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btCadProduto.Location = new System.Drawing.Point(0, 89);
+            this.btCadProduto.Name = "btCadProduto";
+            this.btCadProduto.Size = new System.Drawing.Size(141, 23);
+            this.btCadProduto.TabIndex = 5;
+            this.btCadProduto.Text = "Cadastro";
+            this.btCadProduto.UseVisualStyleBackColor = true;
+            // 
+            // btEdiProduto
+            // 
+            this.btEdiProduto.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btEdiProduto.Location = new System.Drawing.Point(0, 109);
+            this.btEdiProduto.Name = "btEdiProduto";
+            this.btEdiProduto.Size = new System.Drawing.Size(141, 23);
+            this.btEdiProduto.TabIndex = 6;
+            this.btEdiProduto.Text = "Edição";
+            this.btEdiProduto.UseVisualStyleBackColor = true;
+            // 
+            // btEdiVenda
+            // 
+            this.btEdiVenda.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btEdiVenda.Location = new System.Drawing.Point(0, 312);
+            this.btEdiVenda.Name = "btEdiVenda";
+            this.btEdiVenda.Size = new System.Drawing.Size(141, 23);
+            this.btEdiVenda.TabIndex = 7;
+            this.btEdiVenda.Text = "Edição";
+            this.btEdiVenda.UseVisualStyleBackColor = true;
+            // 
+            // btEdiCliente
+            // 
+            this.btEdiCliente.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btEdiCliente.Location = new System.Drawing.Point(0, 210);
+            this.btEdiCliente.Name = "btEdiCliente";
+            this.btEdiCliente.Size = new System.Drawing.Size(141, 23);
+            this.btEdiCliente.TabIndex = 8;
+            this.btEdiCliente.Text = "Edição";
+            this.btEdiCliente.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(141, 452);
+            this.panel1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btCadVenda);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btCadCliente);
+            this.Controls.Add(this.btCadProduto);
+            this.Controls.Add(this.btEdiProduto);
+            this.Controls.Add(this.btEdiVenda);
+            this.Controls.Add(this.btEdiCliente);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sistema de Ponto de Venda";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btCadVenda;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btCadCliente;
+        private System.Windows.Forms.Button btCadProduto;
+        private System.Windows.Forms.Button btEdiProduto;
+        private System.Windows.Forms.Button btEdiVenda;
+        private System.Windows.Forms.Button btEdiCliente;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
